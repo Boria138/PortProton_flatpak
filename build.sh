ec91d06c5ba34fd2dcacf481c1fde9c015d81ce6
Boria138 <boriabloger@protonmail.com>
2024-01-29 22:20:35 +0600

    Added ffmpeg

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 git submodule init
 git submodule update
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install --user flathub org.gnome.Platform.Compat.i386//45 org.freedesktop.Platform.GL32.default//23.08 org.freedesktop.Platform.GL32.default//23.08-extra org.winehq.Wine.mono//stable-23.08 org.winehq.Wine.gecko//stable-23.08  -y
+flatpak install --user flathub org.gnome.Platform.Compat.i386//45 org.freedesktop.Platform.GL32.default//23.08 org.freedesktop.Platform.GL32.default//23.08-extra org.winehq.Wine.mono//stable-23.08 org.winehq.Wine.gecko//stable-23.08 org.freedesktop.Platform.ffmpeg-full//23.08 org.freedesktop.Platform.ffmpeg_full.i386//23.08  -y
 if [[ -d /sys/module/nvidia ]]
 then flatpak install --user flathub org.freedesktop.Platform.GL32.nvidia-535-154-05 -y
 fi
