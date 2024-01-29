2d7a70f8bcff14566a666630ae21916cebf3ee7e
Boria138 <boriabloger@protonmail.com>
2024-01-29 13:14:56 +0600

    Big rewrite

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 git submodule init
 git submodule update
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install --user flathub org.gnome.Platform.Compat.i386//45 org.freedesktop.Platform.GL32.default//23.08 org.freedesktop.Platform.GL32.default//23.08-extra  -y
+if [[ -d /sys/module/nvidia ]]
+then flatpak install --user flathub org.freedesktop.Platform.GL32.nvidia-535-154-05 -y
+fi
 flatpak-builder --user --repo=local --force-clean --install-deps-from=flathub build-dir com.castrofidel.portproton.yml
 flatpak --user remote-add --no-gpg-verify --if-not-exists local local
-flatpak install local com.castrofidel.portproton
+flatpak install local com.castrofidel.portproton -y
